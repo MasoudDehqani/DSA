948b237648100f0730c0f9b0d2da6df6d987c65d
Masoud <masoud67.md@gmail.com>
2025-01-24 09:50:55 +0330

    OCaml: an implementation of stack data structure added

--- OCaml/src/data_structures/stack.ml
+++ OCaml/src/data_structures/stack.ml
@@ -1,1 +1,29 @@
-let v = ()
+module type Stack = sig
+  type 'a stack  
+  val empty: 'a stack
+  
+  val push: 'a -> 'a stack -> 'a stack
+
+  val peek: 'a stack -> 'a option
+  
+  val pop: 'a stack -> 'a option
+end
+
+module Stack: Stack = struct
+  
+  type 'a stack = 
+    | Empty
+    | Entry of 'a * 'a stack
+
+  let empty = Empty
+  
+  let push v s = Entry (v, s)
+  
+  let peek = function
+    | Empty -> None
+    | Entry (v, _) -> Some(v)
+  
+  let pop = function
+    | Empty -> None
+    | Entry (v, _) -> Some(v)
+end

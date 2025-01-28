604096d5bf406c90938a0c6b98cdba793a05682b
Masoud <masoud67.md@gmail.com>
2025-01-28 21:39:43 +0330

    rescript: more operations added to singly linked list, ocamlformat configured

---
+++ OCaml/README.md
@@ -0,0 +1,4 @@
+Run 
+```eval $(opam env)``` 
+every time before running commands related to OCaml eg:
+```echo "version = `ocamlformat --version`" > .ocamlformat```
--- OCaml/src/DataStructures/SinglyLinkedList.ml
+++ OCaml/src/DataStructures/SinglyLinkedList.ml
@@ -1,13 +1,14 @@
-  type 'a t =
-    | Nil
-    | Node of 'a * 'a t
-  
-  let rec read index = function
-    | Nil -> None
-    | Node (head, tail) -> if index = 0 then Some(head) else read (index - 1) tail
+type 'a t = Nil | Node of 'a * 'a t
 
-  let rec search_aux value ?(current_index=0) lst = match lst with
+let rec read index = function
   | Nil -> None
-  | Node (head, tail) -> if head = value then Some(current_index) else search_aux value tail ~current_index:(current_index + 1)
+  | Node (head, tail) -> if index = 0 then Some head else read (index - 1) tail
 
-  let search value lst = search_aux value lst ~current_index:0
+let rec search_aux value ?(current_index = 0) lst =
+  match lst with
+  | Nil -> None
+  | Node (head, tail) ->
+      if head = value then Some current_index
+      else search_aux value tail ~current_index:(current_index + 1)
+
+let search value lst = search_aux value lst ~current_index:0

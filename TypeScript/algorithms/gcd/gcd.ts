81ec6b57fe25f5a0cc6acd5d827fc1088c5b98cf
Masoud <masoud67.md@gmail.com>
2025-02-07 19:51:02 +0330

    TypeScript: add various implementations of gcd and pow algorithms

--- TypeScript/algorithms/gcd/gcd.ts
+++ TypeScript/algorithms/gcd/gcd.ts
@@ -1,6 +1,11 @@
-// export default function gcd(n1: number, n2: number): number {
-//   return n2 === 0 ? n1 : gcd(n2, n1 % n2);
-// }
+// const gcd = (n1: number, n2: number) => {
+//   if (n1 === n2) return n1;
+//   if (n1 === 0) return n2;
+//   if (n2 === 0) return n1;
+
+//   if (n1 > n2) return gcd(n1 % n2, n2);
+//   return gcd(n2 % n1, n1);
+// };
 
 const gcd = (n1: number, n2: number): number =>
   n2 === 0 ? n1 : gcd(n2, n1 % n2);
--- TypeScript/algorithms/pow/pow.ts
+++ TypeScript/algorithms/pow/pow.ts
@@ -1,11 +1,25 @@
-export default function pow(n: number, p: number): number {
-  if (p === 0) return 1;
-  if (n === 0) return 0;
+export function powRecursive(n: number, exp: number): number {
+  if (exp === 0) return 1;
 
-  if (p < 0) return 1 / pow(n, -p);
+  return exp > 0 ? powRecursive(n, exp - 1) * n : powRecursive(n, exp + 1) / n;
+}
 
-  const halfPower = pow(n, Math.trunc(p / 2));
-  const res = halfPower * halfPower;
+const powTailRecursiveAux = (n: number, exp: number, acc: number): number => {
+  if (exp === 0) return acc;
+  return powTailRecursiveAux(n, exp - 1, acc * n);
+};
 
-  return p % 2 === 0 ? res : res * n;
+export function powTailRecursive(n: number, exp: number): number {
+  const res = powTailRecursiveAux(n, exp >= 0 ? exp : -exp, 1);
+  return exp > 0 ? res : 1 / res;
+}
+
+export function powTailRecursiveSquaring(n: number, exp: number): number {
+  if (exp === 0) return 1;
+  if (exp < 0) return 1 / powTailRecursiveSquaring(n, -exp);
+
+  const half = powTailRecursiveAux(n, Math.floor(exp / 2), 1);
+  const squared = half * half;
+
+  return exp % 2 === 0 ? squared : squared * n;
 }

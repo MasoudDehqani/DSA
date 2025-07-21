84eb5e387f623478fe772b52461e7f60d1504755
Masoud <masoud67.md@gmail.com>
2025-07-21 05:30:11 +0330

    TypeScript: insertion sort improved

--- TypeScript/algorithms/sort/insertionSort.ts
+++ TypeScript/algorithms/sort/insertionSort.ts
@@ -1,11 +1,29 @@
-export default function insertionSort(arr: Array<number>, arrayLength: number) {
-  for (let i = 1; i < arrayLength; i++) {
-    const key = arr[i];
-    for (let j = i - 1; j >= 0; j--) {
-      if (key < arr[j]) {
-        [arr[j], arr[j + 1]] = [key, arr[j]];
-      }
+// export default function insertionSort(arr: Array<number>, arrayLength: number) {
+//   for (let i = 1; i < arrayLength; i++) {
+//     const key = arr[i];
+//     for (let j = i - 1; j >= 0; j--) {
+//       if (key < arr[j]) {
+//         [arr[j], arr[j + 1]] = [key, arr[j]];
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
+export default function insertionSort(arr: Array<number>) {
+  
+  for (let i = 1; i < arr.length; i++) {
+
+    const key = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && key < arr[j]) {
+      arr[j + 1] = arr[j]
+      j--
     }
+
+    arr[j + 1] = key
   }
 
   return arr;
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,3 +1,3 @@
 import insertionSort from "./algorithms/sort/insertionSort.js";
 
-console.log(insertionSort([2, 3, 1, 9, 0, 8], 6))
+console.log(insertionSort([2, 3, 1, 9, 0, 8]))

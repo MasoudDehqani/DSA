962b7ef15c9ab6ce18e3e9b48d187ef435fff6e8
Masoud <masoud67.md@gmail.com>
2025-10-05 16:50:11 +0330

    Rust: a more inefficient version of array rotation algorithm added

--- Rust/src/problems/array/rotate.rs
+++ Rust/src/problems/array/rotate.rs
@@ -1,7 +1,39 @@
-pub fn rotate_right_k_steps(nums: &mut Vec<i32>, mut k: i32) {
-    while k > 0 {
-        let last = nums.remove(nums.len() - 1);
-        nums.insert(0, last);
-        k -= 1;
+// pub fn rotate_right_k_steps(nums: &mut Vec<i32>, mut k: i32) {
+//     while k > 0 {
+//         let last = nums.remove(nums.len() - 1);
+//         nums.insert(0, last);
+//         k -= 1;
+//     }
+// }
+
+pub fn rotate_right_k_steps(nums: &mut Vec<i32>, k: i32) {
+    let k = (k as usize) % nums.len();
+    // k = if k > nums.len() { k % nums.len() } else { k };
+    // if k == nums.len() {
+    //     return;
+    // }
+
+    let mut i = 0;
+    let mut j = nums.len() - k;
+    let mut temp = Vec::with_capacity(nums.len() - k);
+
+    while i < nums.len() {
+        if i < k {
+            temp.push(nums[i]);
+            nums[i] = nums[j];
+        } else {
+            if i < nums.len() - k {
+                temp.push(nums[i]);
+            }
+        }
+
+        i += 1;
+        j += 1;
+    }
+
+    i = 0;
+    for l in k..nums.len() {
+        nums[l] = temp[i];
+        i += 1;
     }
 }

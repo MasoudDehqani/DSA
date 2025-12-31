e96a880ad21454439a58979d4a4cb25326901ad4
Masoud <masoud67.md@gmail.com>
2025-12-31 07:46:57 +0330

    Rust: a better version of optimized pascal_triangle_at added

--- Rust/src/problems/pascal_triangle.rs
+++ Rust/src/problems/pascal_triangle.rs
@@ -28,35 +28,57 @@ pub fn pascal_triangle_at(row: u32, column: u32) -> u32 {
     pascal_triangle_at(row - 1, column) + pascal_triangle_at(row - 1, column - 1)
 }
 
-pub fn pascal_triangle_at_mem(row: u32, column: u32) -> u32 {
+// pub fn pascal_triangle_at_mem(row: u32, column: u32) -> u32 {
+//     let mut memo = HashMap::new();
+
+//     fn aux(r: u32, c: u32, mem: &mut HashMap<(u32, u32), u32>) -> u32 {
+//         if r < 2 || (c < 1 || c >= r) {
+//             return 1;
+//         }
+
+//         let top_right_key = (r - 1, c);
+//         let maybe_v1 = mem.get(&top_right_key);
+//         let v1 = match maybe_v1 {
+//             Some(v) => *v,
+//             None => {
+//                 let v = aux(top_right_key.0, top_right_key.1, mem);
+//                 mem.insert(top_right_key, v);
+//                 v
+//             }
+//         };
+
+//         let top_left_key = (r - 1, c - 1);
+//         let maybe_v2 = mem.get(&top_left_key);
+//         let v2 = match maybe_v2 {
+//             Some(v) => *v,
+//             None => {
+//                 let v = aux(top_left_key.0, top_left_key.1, mem);
+//                 mem.insert(top_left_key, v);
+//                 v
+//             }
+//         };
+
+//         v1 + v2
+//     }
+
+//     aux(row, column, &mut memo)
+// }
+
+pub fn pascal_triangle_at_mem(row: u32, column: u32) -> u64 {
     let mut memo = HashMap::new();
 
-    fn aux(r: u32, c: u32, mem: &mut HashMap<(u32, u32), u32>) -> u32 {
-        if r < 2 || (c < 1 || c >= r) {
+    fn aux(r: u32, c: u32, mem: &mut HashMap<(u32, u32), u64>) -> u64 {
+        if c == 0 || c == r {
             return 1;
         }
 
-        let maybe_v1 = mem.get(&(r - 1, c));
-        let v1 = match maybe_v1 {
-            Some(v) => *v,
-            None => {
-                let v = aux(r - 1, c, mem);
-                mem.insert((r - 1, c), v);
-                v
-            }
-        };
+        if let Some(&v) = mem.get(&(r, c)) {
+            return v;
+        }
 
-        let maybe_v2 = mem.get(&(r - 1, c - 1));
-        let v2 = match maybe_v2 {
-            Some(v) => *v,
-            None => {
-                let v = aux(r - 1, c - 1, mem);
-                mem.insert((r - 1, c - 1), v);
-                v
-            }
-        };
-
-        v1 + v2
+        let v = aux(r - 1, c, mem) + aux(r - 1, c - 1, mem);
+        mem.insert((r, c), v);
+        v
     }
 
     aux(row, column, &mut memo)

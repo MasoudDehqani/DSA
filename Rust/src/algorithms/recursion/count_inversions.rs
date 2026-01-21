8806224a2c596252ff6632d3894bdbc78e07b430
Masoud <masoud67.md@gmail.com>
2026-01-21 07:07:07 +0330

    Rust: count inversions added

--- Rust/src/algorithms/recursion/count_inversions.rs
+++ Rust/src/algorithms/recursion/count_inversions.rs
@@ -1,3 +1,29 @@
+// pub fn count_inversions(arr: &Vec<i32>) -> i32 {
+//     fn aux(arr: &Vec<i32>, i: usize, pair: &mut Vec<i32>, count: &mut i32) {
+//         if pair.len() == 2 {
+//             if pair[0] > pair[1] {
+//                 *count += 1;
+//             }
+
+//             return;
+//         }
+
+//         if i >= arr.len() {
+//             return;
+//         }
+
+//         pair.push(arr[i]);
+//         aux(arr, i + 1, pair, count);
+
+//         pair.pop();
+//         aux(arr, i + 1, pair, count);
+//     }
+
+//     let mut count = 0;
+//     aux(arr, 0, &mut vec![], &mut count);
+//     count
+// }
+
 pub fn count_inversions(arr: &Vec<i32>) -> i32 {
     fn aux(arr: &Vec<i32>, i: usize, pair: &mut Vec<i32>) -> i32 {
         if pair.len() == 2 {
